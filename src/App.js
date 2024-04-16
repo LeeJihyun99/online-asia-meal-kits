38e7d3e7f69ec263c429a59b1119f72fe04e7bc4
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-04-16 15:58:19 +0200

    added routes components and starting organizing the app based on it

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import koreaMain from './images/categories-main/category-main-korea.jpg';
-import vietnamMain from './images/categories-main/category-main-vietnam.jpg';
-import chinaMain from './images/categories-main/category-main-china.jpg';
-import japanMain from './images/categories-main/category-main-japan.jpg';
-import others from './images/categories-main/category-main-others.jpg';
-import Directory from './components/directory/directory.component';
+import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom';
 
 const App= () =>{
-  const categories = [
-    {
-      id: 1,
-      title: 'Korea',
-      imageURL: koreaMain 
-  
-    },
-    {
-      id: 2,
-      title: 'Vietnam',
-      imageURL: vietnamMain
-    },
-    {
-      id: 3,
-      title: 'China',
-      imageURL:chinaMain
-    },
-    {
-      id: 4,
-      title: 'Japan',
-      imageURL: japanMain
-    },
-    {
-      id:5,
-      title:'Others',
-      imageURL: others
-    }
-  ];
-
-  return (
-    <Directory categories={categories}/>
-  );
-};
-
+  return(
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+  )
+}
 export default App;
 
