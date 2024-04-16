4acbac4adad43dc30988f7dad7b4738334e7011f
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-04-16 15:12:56 +0200

    added scss files for each component & added componentes for optimization

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import koreaMain from '../src/images/categories-main/category-main-korea.jpg';
-import vietnamMain from '../src/images/categories-main/category-main-vietnam.jpg';
-import chinaMain from '../src/images/categories-main/category-main-china.jpg';
-import japanMain from '../src/images/categories-main/category-main-japan.jpg';
+import koreaMain from './images/categories-main/category-main-korea.jpg';
+import vietnamMain from './images/categories-main/category-main-vietnam.jpg';
+import chinaMain from './images/categories-main/category-main-china.jpg';
+import japanMain from './images/categories-main/category-main-japan.jpg';
+import others from './images/categories-main/category-main-others.jpg';
+import Directory from './components/directory/directory.component';
 
 const App= () =>{
+  const categories = [
+    {
+      id: 1,
+      title: 'Korea',
+      imageURL: koreaMain 
+  
+    },
+    {
+      id: 2,
+      title: 'Vietnam',
+      imageURL: vietnamMain
+    },
+    {
+      id: 3,
+      title: 'China',
+      imageURL:chinaMain
+    },
+    {
+      id: 4,
+      title: 'Japan',
+      imageURL: japanMain
+    },
+    {
+      id:5,
+      title:'Others',
+      imageURL: others
+    }
+  ];
+
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        <img src={koreaMain} width={350} height={300} alt='korea'/>
-        <div className="category-body-container">
-          <h1>Korean</h1>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src={vietnamMain} width={350} height={300} alt='vietnam'/>
-        <div className="category-body-container">
-          <h1>Vietnamese</h1>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src={chinaMain} width={350} height={300} alt='china'/>
-        <div className="category-body-container">
-          <h1>Chinese</h1>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <img src={japanMain} width={350} height={300} alt='japan'/>
-        <div className="category-body-container">
-          <h1>Japanese</h1>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <Directory categories={categories}/>
+  );
+};
 
 export default App;
+
