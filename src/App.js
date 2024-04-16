4c290687558f0a22b23580c33a81941d654a6582
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-04-16 17:22:45 +0200

    added icon for the shoppig bag and adjusted its position

--- src/App.js
+++ src/App.js
@@ -8,12 +8,33 @@ const Shop = ()=>{
   )
 };
 
+const Contact = ()=> {
+  return(
+    <h2>I'm the contact page. </h2>
+  )
+}
+
+const Signin = ()=> {
+  return(
+    <h2>I'm the registration page.</h2>
+  )
+}
+
+const Buy = ()=> {
+  return(
+    <h2>I'm the bag page.</h2>
+  )
+}
+
 const App= () =>{
   return(
     <Routes>
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
         <Route path='shop' element={<Shop/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='Signin' element={<Signin/>}/>
+        <Route path='Buy' element={<Buy/>}/>
       </Route>
     </Routes>
   )
--- src/routes/navigation/naviation.component.jsx
+++ src/routes/navigation/naviation.component.jsx
@@ -2,6 +2,7 @@ import { Outlet,Link } from "react-router-dom";
 import { Fragment } from "react";
 import {ReactComponent as Logo} from '../../assets/logo.svg';
 import './navigation.styles.scss';
+import {ReactComponent as ShoppingBag} from '../../assets/shopping-bag.svg';
 
 const Navigation = ()=> {
     return(
@@ -13,6 +14,21 @@ const Navigation = ()=> {
         <div className="nav-links-container">
             <Link className="nav-link" to='/shop'>
                 SHOP
+            </Link>
+        </div>
+        <div className="nav-links-container">
+            <Link className="nav-link" to='/contact'>
+                CONTACT
+            </Link>
+        </div>
+        <div className="nav-links-container">
+            <Link className="nav-link" to='/signin'>
+                SIGN IN
+            </Link>
+        </div>
+        <div className="nav-links-container">
+            <Link className="nav-link" to='/buy'>
+                <ShoppingBag className="shopping-bag-icon"/>
             </Link>
         </div>
         </div>
